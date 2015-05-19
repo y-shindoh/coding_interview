98d8848cc98b7ef84e770e2f5d3d4d1485e215d0
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-05-19 20:02:17 +0900

    Add comments.

--- c_04/q_04.02.cpp
+++ c_04/q_04.02.cpp
@@ -10,16 +10,20 @@
 #include <cstdio>
 #include <set>
 #include <vector>
-#include "binary_search_tree.hpp"
 
 /**
- * 比較関数
+ * 探索処理
+ * @param	table	有向グラフのテーブル
+ * @param	start	始点
+ * @param	goal	終点
+ * @param	buffer	探索用作業領域
+ * @note	深さ優先探索で実装している。無向グラフならunion-find treeが有利。
  */
 bool
 search(std::vector< std::set<size_t> >& table,
 	   size_t start,
 	   size_t goal,
-	   std::set<size_t>& used)
+	   std::set<size_t>& buffer)
 {
 	if (start == goal) return true;
 
@@ -27,10 +31,10 @@ search(std::vector< std::set<size_t> >& table,
 	std::set<size_t>::const_iterator it;
 
 	for (it = table[start].begin(); it != table[start].end(); ++it) {
-		if (used.find(*it) != used.end()) continue;
-		used.insert(*it);
-		flag = search(table, *it, goal, used);
-		used.erase(*it);
+		if (buffer.find(*it) != buffer.end()) continue;
+		buffer.insert(*it);
+		flag = search(table, *it, goal, buffer);
+		buffer.erase(*it);
 		if (flag) return true;
 	}
 
@@ -42,28 +46,22 @@ search(std::vector< std::set<size_t> >& table,
  */
 int main()
 {
-	std::vector< std::set<size_t> > table;
-	std::set<size_t> used;
+	std::vector< std::set<size_t> > table;	// 有向グラフのテーブル
+	std::set<size_t> buffer;	// 探索用作業領域
 
-	table.resize(5);
-	table[0].insert(2);
-	table[2].insert(1);
-	table[1].insert(4);
+	table.resize(6);	// ノード数6 (0〜5)
+	table[0].insert(2);	// 0 => 2
+	table[2].insert(1);	// 2 => 1
+	table[1].insert(4);	// 1 => 4
 
-	if (search(table, 0, 5, used)) {
-		std::printf("OK\n");
-	}
-	else {
-		std::printf("NG\n");
+	if (!search(table, 0, 5, buffer)) {
+		std::printf("NG\n");	// 到達不可
 	}
 
-	table[4].insert(5);
+	table[4].insert(5);	// 4 => 5
 
-	if (search(table, 0, 5, used)) {
-		std::printf("OK\n");
-	}
-	else {
-		std::printf("NG\n");
+	if (search(table, 0, 5, buffer)) {
+		std::printf("OK\n");	// 到達可
 	}
 
 	return 0;
