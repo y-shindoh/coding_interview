f8170c1ff0f29747cc99953fac95ea2df441db74
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-05-24 14:17:45 +0900

    Add a comment.

--- c_05/q_05.04.cpp
+++ c_05/q_05.04.cpp
@@ -18,10 +18,12 @@ int main()
 	unsigned int x(0xF0F0F0F0);
 	unsigned int y(0x08000000);
 
+	// 1のビットのうち最下位をクリア
 	print_bits<unsigned int>(stdout, x);
 	print_bits<unsigned int>(stdout, x - 1);
 	print_bits<unsigned int>(stdout, x & (x - 1));
 
+	// 1のビットが1つしかないと0になる
 	print_bits<unsigned int>(stdout, y);
 	print_bits<unsigned int>(stdout, y - 1);
 	print_bits<unsigned int>(stdout, y & (y - 1));
