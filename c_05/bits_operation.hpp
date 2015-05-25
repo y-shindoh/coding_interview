312d7d95e58bab4e22942fd091dd943c48e0f45d
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-05-25 18:20:58 +0900

    Add a new file for Question 5.3.

--- c_05/bits_operation.hpp
+++ c_05/bits_operation.hpp
@@ -13,6 +13,8 @@
 #include <cstdio>
 #include <cassert>
 
+typedef unsigned long long int bo_uint;
+
 /**
  * ビットの状況を出力
  * @param[out]	file	出力先ファイル
@@ -24,16 +26,20 @@ void
 print_bits(FILE* file,
 		   TYPE bits)
 {
-	TYPE h, k;
+	size_t h, k;
 	int c;
 
 	for (size_t i(0); i < sizeof(TYPE); ++i) {
 		if (0 < i) std::fputc((int)' ', file);
-		h = sizeof(TYPE) - i - 1;	// for little endian
+#ifdef	__LITTLE_ENDIAN__
+		h = sizeof(TYPE) - i - 1;
+#else	// __LITTLE_ENDIAN__
+		h = i;
+#endif	// __LITTLE_ENDIAN__
 		for (size_t j(0); j < 8; ++j) {
 			k = 8 - j - 1;
-			c = '0';
-			if (bits & ((TYPE)1 << (h * 8 + k))) c = '1';
+			c = (int)'0';
+			if (bits & ((TYPE)1 << (h * 8 + k))) c = (int)'1';
 			std::fputc(c, file);
 		}
 	}
@@ -152,14 +158,14 @@ template<typename TYPE>
 TYPE
 get_highest_bit(TYPE bits)
 {
-	size_t v = (size_t)bits;
+	bo_uint v = (bo_uint)bits;
 
-	v = ((size_t)0xFFFFFFFF00000000 & v) ? ((size_t)0xFFFFFFFF00000000 & v) : v;
-	v = ((size_t)0xFFFF0000FFFF0000 & v) ? ((size_t)0xFFFF0000FFFF0000 & v) : v;
-	v = ((size_t)0xFF00FF00FF00FF00 & v) ? ((size_t)0xFF00FF00FF00FF00 & v) : v;
-	v = ((size_t)0xF0F0F0F0F0F0F0F0 & v) ? ((size_t)0xF0F0F0F0F0F0F0F0 & v) : v;
-	v = ((size_t)0xCCCCCCCCCCCCCCCC & v) ? ((size_t)0xCCCCCCCCCCCCCCCC & v) : v;
-	v = ((size_t)0xAAAAAAAAAAAAAAAA & v) ? ((size_t)0xAAAAAAAAAAAAAAAA & v) : v;
+	v = ((bo_uint)0xFFFFFFFF00000000 & v) ? ((bo_uint)0xFFFFFFFF00000000 & v) : v;
+	v = ((bo_uint)0xFFFF0000FFFF0000 & v) ? ((bo_uint)0xFFFF0000FFFF0000 & v) : v;
+	v = ((bo_uint)0xFF00FF00FF00FF00 & v) ? ((bo_uint)0xFF00FF00FF00FF00 & v) : v;
+	v = ((bo_uint)0xF0F0F0F0F0F0F0F0 & v) ? ((bo_uint)0xF0F0F0F0F0F0F0F0 & v) : v;
+	v = ((bo_uint)0xCCCCCCCCCCCCCCCC & v) ? ((bo_uint)0xCCCCCCCCCCCCCCCC & v) : v;
+	v = ((bo_uint)0xAAAAAAAAAAAAAAAA & v) ? ((bo_uint)0xAAAAAAAAAAAAAAAA & v) : v;
 
 	return (TYPE)v;
 }
@@ -174,16 +180,60 @@ template<typename TYPE>
 size_t
 count_bits(TYPE bits)
 {
-	size_t v = (size_t)bits;
+	bo_uint v = (bo_uint)bits;
 
-	v = (((size_t)0xAAAAAAAAAAAAAAAA & v) >> 1) + ((size_t)0x5555555555555555 & v);
-	v = (((size_t)0xCCCCCCCCCCCCCCCC & v) >> 2) + ((size_t)0x3333333333333333 & v);
-	v = (((size_t)0xF0F0F0F0F0F0F0F0 & v) >> 4) + ((size_t)0x0F0F0F0F0F0F0F0F & v);
-	v = (((size_t)0xFF00FF00FF00FF00 & v) >> 8) + ((size_t)0x00FF00FF00FF00FF & v);
-	v = (((size_t)0xFFFF0000FFFF0000 & v) >> 16) + ((size_t)0x0000FFFF0000FFFF & v);
-	v = (((size_t)0xFFFFFFFF00000000 & v) >> 32) + ((size_t)0x00000000FFFFFFFF & v);
+	v = (((bo_uint)0xAAAAAAAAAAAAAAAA & v) >> 1) + ((bo_uint)0x5555555555555555 & v);
+	v = (((bo_uint)0xCCCCCCCCCCCCCCCC & v) >> 2) + ((bo_uint)0x3333333333333333 & v);
+	v = (((bo_uint)0xF0F0F0F0F0F0F0F0 & v) >> 4) + ((bo_uint)0x0F0F0F0F0F0F0F0F & v);
+	v = (((bo_uint)0xFF00FF00FF00FF00 & v) >> 8) + ((bo_uint)0x00FF00FF00FF00FF & v);
+	v = (((bo_uint)0xFFFF0000FFFF0000 & v) >> 16) + ((bo_uint)0x0000FFFF0000FFFF & v);
+	v = (((bo_uint)0xFFFFFFFF00000000 & v) >> 32) + ((bo_uint)0x00000000FFFFFFFF & v);
 
-	return v;
+	return (size_t)v;
+}
+
+/**
+ * ビットのうち @a 1 になっているものの総数が等しい次に大きな整数を算出
+ * @param[in]	bits	処理対象の整数
+ * @return	次に大きな整数を算出
+ * @note	テンプレートの型 @a TYPE は @a int 以上の幅の符号なし整数。
+ * @todo	もっと良い方法を考えること。
+ */
+template<typename TYPE>
+TYPE
+next_same_bit_counts(TYPE bits)
+{
+	for (size_t i(0); i < sizeof(TYPE) * 8 - 1; ++i) {
+		if (!(bits & ((TYPE)1 << i))) continue;
+		if (bits & ((TYPE)1 << (i+1))) continue;
+		bits &= ~((TYPE)1 << i);
+		bits |= (TYPE)1 << (i + 1);
+		return bits;
+	}
+
+	return bits;
+}
+
+/**
+ * ビットのうち @a 1 になっているものの総数が等しい次に小さな整数を算出
+ * @param[in]	bits	処理対象の整数
+ * @return	次に小さな整数を算出
+ * @note	テンプレートの型 @a TYPE は @a int 以上の幅の符号なし整数。
+ * @todo	もっと良い方法を考えること。
+ */
+template<typename TYPE>
+TYPE
+previous_same_bit_counts(TYPE bits)
+{
+	for (size_t i(0); i < sizeof(TYPE) * 8 - 1; ++i) {
+		if (bits & ((TYPE)1 << i)) continue;
+		if (!(bits & ((TYPE)1 << (i+1)))) continue;
+		bits |= (TYPE)1 << i;
+		bits &= ~((TYPE)1 << (i + 1));
+		return bits;
+	}
+
+	return bits;
 }
 
 #endif	// __BITS_OPERATION_HPP__
