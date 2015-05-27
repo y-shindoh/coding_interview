79d9f08384d5f1c4ef9cd0ba33a8ca3346b3256f
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-05-27 18:45:58 +0900

    Fix a mistake about type "int".

--- c_03/q_03.04.cpp
+++ c_03/q_03.04.cpp
@@ -19,7 +19,60 @@ class HanoiSolver
 private:
 
 	size_t count_;	///< おもりの移動回数
-	std::vector<unsigned int> towers_[3];	///< 3つの塔
+	std::vector<unsigned int> towers_[3];	///< 3つの塔 (スタック)
+
+	/**
+	 * ハノイの塔の問題を解決 (補助関数)
+	 * @param[in]	n	移動させるおもりの数
+	 * @param[in]	f	移動元の塔
+	 * @param[in]	w	作業用の塔
+	 * @param[in]	d	移動先の塔
+	 * @note	呼び出し時に各nに対して塔が初期状態 (説明が難しい) であれば、
+				関数が返るときには、
+				@a f の塔から @a d の塔に @a n 個の重りが移動している。
+	 */
+	void
+	execute(int n,
+			int f,
+			int w,
+			int d)
+		{
+			if (n <= 0) {
+				std::printf("----\n");
+				return;
+			}
+
+			// fからwにn-1枚を移動 
+			execute(n - 1, f, d, w);
+
+			print();
+			++count_;
+
+			// nに対して残りの1枚を移動
+			unsigned int v = towers_[f].back();
+			towers_[f].pop_back();
+			towers_[d].push_back(v);
+
+			// wからdにn-1枚を移動 
+			execute(n - 1, w, f, d);
+		}
+
+	/**
+	 * ハノイの塔の状態を出力
+	 */
+	void
+	print() const
+		{
+			for (size_t i(0); i < 3; ++i) {
+				std::printf("[%lu] ", i);
+				size_t l = towers_[i].size();
+				for (size_t j(0); j < l; ++j) {
+					if (0 < j) std::printf(", ");
+					std::printf("%u", towers_[i][j]);
+				}
+				std::printf("\n");
+			}
+		}
 
 public:
 
@@ -48,53 +101,6 @@ public:
 			print();
 
 			std::printf("=> %lu\n", count_);
-		}
-
-	/**
-	 * ハノイの塔の問題を解決
-	 * @param[in]	n	先の再帰処理の回数
-	 * @param[in]	f	移動元の塔
-	 * @param[in]	w	作業用の塔
-	 * @param[in]	d	移動先の塔
-	 */
-	void
-	execute(int n,
-			int f,
-			int w,
-			int d)
-		{
-			if (n <= 0) {
-				std::printf("----\n");
-				return;
-			}
-
-			execute(n - 1, f, d, w);
-
-			print();
-			++count_;
-
-			unsigned int v = towers_[f].back();
-			towers_[f].pop_back();
-			towers_[d].push_back(v);
-
-			execute(n - 1, w, f, d);
-		}
-
-	/**
-	 * ハノイの塔の状態を出力
-	 */
-	void
-	print()
-		{
-			for (size_t i(0); i < 3; ++i) {
-				std::printf("[%lu] ", i);
-				size_t l = towers_[i].size();
-				for (size_t j(0); j < l; ++j) {
-					if (0 < j) std::printf(", ");
-					std::printf("%d", towers_[i][j]);
-				}
-				std::printf("\n");
-			}
 		}
 };
 
