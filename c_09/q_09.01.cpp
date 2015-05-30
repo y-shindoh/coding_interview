2ad28f512a74931648f915337597b4d1fbf6a0cf
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-05-30 14:24:10 +0900

    Modify the answer.

--- c_09/q_09.01.cpp
+++ c_09/q_09.01.cpp
@@ -8,39 +8,78 @@
 
 #include <cstddef>
 #include <cstdio>
+#include <cassert>
 #include <vector>
 
+#ifndef	NOT_USE_DP
 /**
- * n段の階段を一歩で1〜N段ずつ登る場合の登り方の数
+ * n段の階段を一歩で1〜N段ずつ登る場合の登り方の数 (DPを用いた実装)
  * @param[in]	n	階段の段数
- * @param[out]	c	登り方の数 (事前に0で初期化すること)
+ * @param[in]	s	一歩で登れる段数
+ * return	登り方の数
  * @note	テンプレートの型 @a TYPE は符号なし整数を指定すること。
- * @note	テンプレートの整数 @a N は一歩で登れる段数を指定すること。
- * @note	分割統治法を併用すれば高速に処理できるが、ここではしない。
  */
-template<typename TYPE, size_t N>
-void
-find_step_sequence(TYPE n, TYPE& c)
+template<typename TYPE>
+TYPE
+find_step_sequence(size_t n,
+				   size_t s)
 {
-	for (size_t i(1); i <= N; ++i) {
+	assert(n);
+	assert(s);
+
+	std::vector<TYPE> count;
+	count.resize(n + 1, (TYPE)0);
+	count[0] = 1;
+
+	for (size_t i(0); i < n; ++i) {
+		for (size_t j(1); j <= s; ++j) {
+			if (n < i + j) break;
+			count[i+j] += count[i];
+		}
+	}
+
+	return count.back();
+}
+#else	// NOT_USE_DP
+/**
+ * n段の階段を一歩で1〜N段ずつ登る場合の登り方の数 (単純な再帰関数の実装)
+ * @param[in]	n	階段の段数
+ * @param[in]	s	一歩で登れる段数
+ * return	登り方の数
+ * @note	テンプレートの型 @a TYPE は符号なし整数を指定すること。
+ */
+template<typename TYPE>
+TYPE
+find_step_sequence(size_t n,
+				   size_t s)
+{
+	assert(n);
+	assert(s);
+
+	TYPE c(0);
+
+	for (size_t i(1); i <= s; ++i) {
 		if (n <= i) {
 			if (n == i) ++c;
 			break;
 		}
-		find_step_sequence<TYPE, N>(n - i, c);
+		c += find_step_sequence<TYPE>(n - i, s);
 	}
+
+	return c;
 }
+#endif	// NOT_USE_DP
 
 /**
  * 動作確認用コマンド
  */
 int main()
 {
-	size_t n(20);
-	size_t c(0);
+	size_t n(50);
+	size_t s(3);
 
-	find_step_sequence<size_t, 3>(n, c);
-	std::printf("%lu / 1-%lu => %lu\n", n , (size_t)3, c);
+	unsigned int c = find_step_sequence<unsigned int>(n, s);
+	std::printf("%lu / 1-%lu => %u\n", n , s, c);
 
 	return 0;
 }
