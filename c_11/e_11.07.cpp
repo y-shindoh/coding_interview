d7dea5e2e5e56f3e273ccb445d22aabcfa7bf779
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-06-03 20:32:27 +0900

    Add sample code about "binary search".

---
+++ c_11/e_11.07.cpp
@@ -0,0 +1,31 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	e_11.07.cpp
+ * @brief	「世界で闘うプログラミング力を鍛える150問」の第11章の例題
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/dp/4839942390 .
+ */
+
+#include <cstddef>
+#include <cassert>
+#include "utility.hpp"
+
+/**
+ * 動作確認用コマンド
+ */
+int main()
+{
+	int data[] = {1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+	const size_t l = sizeof(data) / sizeof(data[0]);
+	size_t i;
+
+	print_array<int>(data, l, "DATA");
+
+	for (int key(3); key < 9; ++key) {
+		i = BinarySearch<int>(data, l, key);	// 二分探索
+		if (i == INVALID_INDEX) continue;
+		std::printf("data[%lu] = %d\n", i, key);
+	}
+
+	return 0;
+}
