d73b597daa38e7ebf663f2c8bc2446f474d7209d
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-06-04 19:00:39 +0900

    Add another answer to Question 9.4.

--- c_09/q_09.04.cpp
+++ c_09/q_09.04.cpp
@@ -35,7 +35,7 @@ private:
 					bool flag)
 		{
 			if (flag) storage_->push_back(buffer_);
-			if (n <= i) return;
+			if (n < i) return;
 			buffer_.push_back(i);
 			execute_routine(n, i + 1, true);
 			buffer_.pop_back();
@@ -50,9 +50,10 @@ public:
 	 * @return	全ての部分集合
 	 * @note	生成失敗時は @a 0 が返却される。
 	 * @note	返却値は本メソッド利用者の責任で削除すること。
+	 * @note	計算量・メモリ占有量ともに O(2^n)。
 	 */
 	std::vector<std::vector<TYPE> >*
-	execute(TYPE n)
+	execute_1(TYPE n)
 		{
 			try {
 				storage_ = new std::vector<std::vector<TYPE> >();
@@ -62,7 +63,40 @@ public:
 			}
 
 			buffer_.clear();
+			buffer_.reserve(n);
 			execute_routine(n, 1, true);
+
+			return storage_;
+		}
+
+	/**
+	 * 全ての部分集合を生成
+	 * @param[in]	n	集合の要素数 (集合は1〜nの要素を含むとみなす)
+	 * @return	全ての部分集合
+	 * @note	生成失敗時は @a 0 が返却される。
+	 * @note	返却値は本メソッド利用者の責任で削除すること。
+	 * @note	計算量・メモリ占有量ともに O(2^n)。
+	 */
+	std::vector<std::vector<TYPE> >*
+	execute_2(TYPE n)
+		{
+			try {
+				storage_ = new std::vector<std::vector<TYPE> >();
+			}
+			catch (...) {
+				return 0;
+			}
+
+			storage_->resize(1);
+
+			size_t m;
+			for (TYPE i(1); i <= n; ++i) {
+				m = storage_->size();
+				for (size_t j(0); j < m; ++j) {
+					storage_->push_back(storage_->at(j));
+					storage_->back().push_back(i);	// 遅い?
+				}
+			}
 
 			return storage_;
 		}
@@ -75,7 +109,7 @@ int main()
 {
 
 	SubsetMaker<unsigned int> maker;
-	std::vector<std::vector<unsigned int> >* subsets = maker.execute(4);
+	std::vector<std::vector<unsigned int> >* subsets = maker.execute_1(3);
 
 	size_t m = subsets->size();
 	size_t n;
