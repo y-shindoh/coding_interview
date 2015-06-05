2c7dd184fbecd2d2fd63fb4b3293a3cca3fcf560
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-06-05 21:18:49 +0900

    Use a cache array.

--- c_09/q_09.10.cpp
+++ c_09/q_09.10.cpp
@@ -16,9 +16,9 @@
  * @param[in,out]	data	タプルの配列
  * @param[in]	length	配列 @a data の要素数
  * @note	ソート条件の優先度はタプルの前方の値ほど強い。
- * @note	実体は手抜きのバブル・ソート。最悪計算量は O(n^2)。
  * @note	テンプレートの型 @a TYPE はタプルの値の型。
  * @note	テンプレートの整数 @a N はタプルの値の数。
+ * @note	実体は手抜きのバブル・ソート。最悪計算量は O(n^2)。
  */
 template<typename TYPE, size_t N>
 void
@@ -28,26 +28,23 @@ sort_tuples(TYPE data[][N],
 	assert(data);
 	assert(length);
 
-	bool c, s;
+	bool c;
 	size_t h;
 	TYPE tmp[N];
 
-	for (size_t i(0); i < length; ++i) {
-		h = length - i;
-		c = false;
-		for (size_t j(1); j < h; ++j) {
-			s = false;
-			for (size_t k(0); k < N; ++k) {
+	for (size_t k(0); k < N; ++k) {
+		for (size_t i(0); i < length; ++i) {
+			h = length - i;
+			c = false;
+			for (size_t j(1); j < h; ++j) {
 				if (data[j-1][k] >= data[j][k]) continue;
-				s = true;
+				c = true;
+				std::memcpy((void*)tmp, (const void*)data[j], sizeof(TYPE) * N);
+				std::memcpy((void*)data[j], (const void*)data[j-1], sizeof(TYPE) * N);
+				std::memcpy((void*)data[j-1], (const void*)tmp, sizeof(TYPE) * N);
 			}
-			if (!s) continue;
-			c = true;
-			std::memcpy((void*)tmp, (const void*)data[j], sizeof(TYPE) * N);
-			std::memcpy((void*)data[j], (const void*)data[j-1], sizeof(TYPE) * N);
-			std::memcpy((void*)data[j-1], (const void*)tmp, sizeof(TYPE) * N);
+			if (!c) break;
 		}
-		if (!c) break;
 	}
 }
 
@@ -80,9 +77,11 @@ print_tuples(FILE* file,
 	if (length) std::fprintf(file, "\n");
 }
 
+size_t buffer[1024];	///< メソッド @a find_tuples_sequence_routine のキャッシュ
+
 /**
  * タプルの完全下位要素を連結
- * @param[in]	data	タプルの配列
+ * @param[in]	data	ソート済みタプルの配列
  * @param[in]	length	配列 @a data の要素数
  * @param[in]	index	最後に繋げた要素
  * @param[in]	target	返却値に用いるタプルの値のインデックス
@@ -103,34 +102,37 @@ find_tuples_sequence_routine(const TYPE data[][N],
 
 	if (length <= index + 1) return 0;
 
-	bool flag;
-	TYPE t;
-	TYPE m(0);
+	if (buffer[index] == ~(size_t)0) {
+		buffer[index] = 0;
+		bool flag;
+		TYPE t;
 
-	for (size_t i(index+1); i < length; ++i) {
-		flag = true;
-		for (size_t j(0); j < N; ++j) {
-			if (data[index][j] > data[i][j]) continue;
-			flag = false;
-			break;
+		for (size_t i(index+1); i < length; ++i) {
+			flag = true;
+			for (size_t j(0); j < N; ++j) {
+				if (data[index][j] > data[i][j]) continue;
+				flag = false;
+				break;
+			}
+			if (!flag) continue;
+			t = find_tuples_sequence_routine<TYPE, N>(data, length, i, target);
+			t += data[i][2];
+			if (buffer[index] < t) buffer[index] = t;
 		}
-		if (!flag) continue;
-		t = find_tuples_sequence_routine<TYPE, N>(data, length, i, target);
-		t += data[i][2];
-		if (m < t) m = t;
 	}
 
-	return m;
+	return buffer[index];
 }
 
 /**
  * タプルを連結し指定値の合計の最大値を算出
- * @param[in]	data	タプルの配列
+ * @param[in]	data	ソート済みタプルの配列
  * @param[in]	length	配列 @a data の要素数
  * @param[in]	target	返却値に用いるタプルの値のインデックス
  * @return	連結したタプルの指定値の最大合計値
  * @note	テンプレートの型 @a TYPE はタプルの値の型。
  * @note	テンプレートの整数 @a N はタプルの値の数。
+ * @todo	ソーティングした方が計算効率が良いか検討すること。
  */
 template<typename TYPE, size_t N>
 TYPE
@@ -164,6 +166,8 @@ int main()
 						 {5, 4, 5},
 						 {2, 3, 4}};
 	const size_t l = sizeof(boxes) / sizeof(boxes[0]);
+
+	std::memset((void*)buffer, ~0, sizeof(buffer));
 
 	print_tuples<size_t, 3>(stdout, boxes, l);
 	sort_tuples<size_t, 3>(boxes, l);
