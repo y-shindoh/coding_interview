1bd6bee1c9dff48dddec6e829823a7c403294e4f
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-07-29 00:29:33 +0900

    Add a file for Question 18.2.

---
+++ c_18/q_18.02.c
@@ -0,0 +1,62 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	q_18.02.c
+ * @brief	「世界で闘うプログラミング力を鍛える150問」の問題18.2の回答
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/dp/4839942390 .
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * 配列を確率的な偏りなく完全にシャッフル
+ * @param[in,out]	data	配列
+ * @param[in]	length	配列 @a data の要素数
+ */
+void shuffle(int data[],
+			 size_t length)
+{
+	srand((unsigned) time(NULL));
+
+	int j;
+	int t;
+	for (size_t i = 0; i < length; ++i) {
+		j = rand() % (length - i) + i;	// 完全な等確率と仮定
+		t = data[i];
+		data[i] = data[j];
+		data[j] = t;
+	}
+}
+
+/**
+ * 配列を標準出力に書き出し
+ * @param[in]	data	配列
+ * @param[in]	length	配列 @a data の要素数
+ */
+void print(const int data[],
+		   size_t length)
+{
+	for (size_t i = 0; i < length; ++i) {
+		if (0 < i) printf(" ");
+		printf("[%lu]%d", i, data[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * 動作確認用コマンド
+ */
+int main()
+{
+	int data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	const size_t l = sizeof(data) / sizeof(data[0]);
+
+	print(data, l);
+	shuffle(data, l);
+	print(data, l);
+
+	return 0;
+}
