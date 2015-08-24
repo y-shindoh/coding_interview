cb57d7abbd2ba947bdf944ff099d7b9116bec666
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-08-25 02:09:47 +0900

    Modify an answer to Question 1.2.

---
+++ c_01/q_01.02.cpp
@@ -0,0 +1,53 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	q_01.02.cpp
+ * @brief	「世界で闘うプログラミング力を鍛える150問」の問題1.2の回答
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/dp/4839942390 .
+ */
+
+/*
+  問題:
+  CかC++で、void reverse(char* str) という
+  '\0' 端末の文字列を逆に並び変える関数を実装してください。
+ */
+
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+
+/**
+ * 文字列を破壊的に反転
+ * @param[in,out]	str	入力文字列・出力文字列
+ */
+void
+reverse(char* s)
+{
+	assert(s);
+
+	int i(0);
+	int j = (int)std::strlen(s) - 1;	// 符号あり整数の方が安全
+	char c;
+
+	while (i < j) {
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+		++i, --j;
+	}
+}
+
+/**
+ * 動作確認用コマンド
+ */
+int main()
+{
+	char buffer[] = "01234 56789.";
+
+	std::printf("BEFORE:\t'%s'\n", buffer);
+	reverse(buffer);
+	std::printf("AFTER:\t'%s'\n", buffer);
+
+	return 0;
+}
