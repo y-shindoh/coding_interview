05efccd0d1114e12554e866a02f132269fe6c9db
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-08-26 23:19:12 +0900

    Modify an comment in Question 2.5.

--- c_02/q_02.05.cpp
+++ c_02/q_02.05.cpp
@@ -6,107 +6,105 @@
  * @note	see http://www.amazon.co.jp/dp/4839942390 .
  */
 
+/*
+  問題:
+  各ノードの要素が1桁の数である連結リストで表された2つの数があります。
+  一の位がリストの先頭になるように、各位の数は逆順に並んでいます。
+  このとき2つの数の和を求め、それを連結リストで表したものを返す関数を書いてください。
+ */
+
 #include <cstddef>
 #include <cstdio>
-#include <cassert>
 #include "list.hpp"
 
 /**
- * リストから数値を生成
- * @param[in]	node	処理対象のリスト (すべての要素は1桁)
- * @param[in]	from_high	リストの並び (true: 上位桁から, false: 下位桁から)
- * @return	生成した数値
- * @note	テンプレートの型 @a TYPE はリストのキーの型。整数型。
- * @note	計算量はO(n)となる。
+ * digitのリストから整数を生成
+ * @param[in]	node	digitのリスト
+ * @param[in]	direction	リストの先頭が1の位か否か
+ * @return	リストから生成した整数
+ * @note	計算量は O(n)。ただし n は引数 @a node のノード数。
  */
 template<typename TYPE>
-TYPE
-MakeNumberFromList(const Node<TYPE>* node,
-				   bool from_high = true)
+size_t
+List2Integer(const Node<TYPE>* node,
+			 bool direction)
 {
-	assert(node);
+	size_t r(0);
 
-	TYPE value((TYPE)0);
-
-	if (from_high) {
+	if (direction) {
+		size_t b(1);
 		while (node) {
-			assert(node->get_key() < (TYPE)10);
-			value *= (TYPE)10;
-			value += node->get_key();
+			r += (size_t)node->get_data() * b;
 			node = node->get_next();
+			b *= 10;
 		}
 	}
 	else {
-		TYPE times((TYPE)1);
 		while (node) {
-			assert(node->get_key() < (TYPE)10);
-			value += node->get_key() * times;
+			r = r * 10 + (size_t)node->get_data();
 			node = node->get_next();
-			times *= (TYPE)10;
 		}
 	}
 
-	return value;
+	return r;
 }
 
 /**
- * 数値からリストを生成
- * @param[in]	number	処理対象の0以上の数値
- * @param[in]	from_high	リストの並び (true: 上位桁から, false: 下位桁から)
- * @return	生成したリスト
- * @note	テンプレートの型 @a TYPE はリストのキーの型。整数型。
- * @note	計算量はO(n)となる。ただしnは引数 @a number の桁数。
+ * 整数からdigitのリストを生成
+ * @param[in]	number	整数
+ * @param[in]	direction	リストの先頭が1の位か否か
+ * @return	digitのリスト (呼び出し側で解放すること)
+ * @note	計算量は O(n)。ただし n は引数 @a number の桁数。
  */
 template<typename TYPE>
 Node<TYPE>*
-MakeListFromNumber(TYPE number,
-				   bool from_high = true)
+Integer2List(size_t number,
+			 bool direction)
 {
-	assert((TYPE)0 <= number);
+	Node<TYPE>* c(0);
 
-	Node<TYPE>* head(0);
-	Node<TYPE>* current(0);
-	Node<TYPE>* other;
-	TYPE key;
+	if (direction) {
+		Node<TYPE>* p(0);
+		Node<TYPE>* r(0);
 
-	while ('-') {
-		key = number % (TYPE)10;
-		other = current;
-		current = new Node<TYPE>(key);
-		if (from_high) {
-			head = current;
-			current->set_next(other);
-		}
-		else {
-			if (!head) head = current;
-			if (other) other->set_next(current);
-		}
-		if (number < (TYPE)10) break;
-		number /= (TYPE)10;
+		do {
+			c = new Node<TYPE>(number % 10);
+			if (p) p->set_next(c);
+			if (!r) r = c;
+			p = c;
+			number /= 10;
+		} while (0 < number);
+
+		return r;
 	}
+	else {
+		do {
+			c = new Node<TYPE>(number % 10, c);
+			number /= 10;
+		} while (0 < number);
 
-	return head;
+		return c;
+	}
 }
 
 /**
- * 数値を示す2つのリストから合計した数値を示すリストを生成
- * @param[in]	node_1	処理対象のリスト (すべての要素は1桁)
- * @param[in]	node_2	処理対象のリスト (すべての要素は1桁)
- * @param[in]	from_high	リストの並び (true: 上位桁から, false: 下位桁から)
- * @return	生成したリスト ([1, 2] + [4, 5] => [5, 7] or [7, 5]
- * @note	テンプレートの型 @a TYPE はリストのキーの型。整数型。
- * @note	計算量はO(n1+n2)となる。ただしn1とn2はそれぞれnode_1とnode_2の長さ。
+ * digitのリスト2つから、その和を表すdigitのリストを生成
+ * @param[in]	input_1	digitのリスト (その1)
+ * @param[in]	input_2	digitのリスト (その2)
+ * @param[in]	direction	リストの先頭が1の位か否か
+ * @return	和を表すdigitのリスト
+ * @note	計算量は O(n)。ただし n は引数 @a input_1 と @a input_2 のノード数の最大値。
  */
 template<typename TYPE>
 Node<TYPE>*
-MakeNumberFrom2Lists(const Node<TYPE>* node_1,
-					 const Node<TYPE>* node_2,
-					 bool from_high = true)
+ListSum(const Node<TYPE>* input_1,
+		const Node<TYPE>* input_2,
+		bool direction = true)
 {
-	TYPE number_1 = MakeNumberFromList<TYPE>(node_1, from_high);
-	TYPE number_2 = MakeNumberFromList<TYPE>(node_2, from_high);
+	size_t n1 = List2Integer<TYPE>(input_1, direction);
+	size_t n2 = List2Integer<TYPE>(input_2, direction);
 
-	return MakeListFromNumber<TYPE>(number_1 + number_2, from_high);
+	return Integer2List<TYPE>(n1 + n2, direction);
 }
 
 /**
@@ -114,22 +112,27 @@ MakeNumberFrom2Lists(const Node<TYPE>* node_1,
  */
 int main()
 {
-	int data_1[] = {1, 3, 5, 7, 9};
-	int data_2[] = {5, 3, 0, 5};
-	Node<int>* list_1 = Node<int>::MakeList(data_1, sizeof(data_1)/sizeof(data_1[0]));
-	Node<int>* list_2 = Node<int>::MakeList(data_2, sizeof(data_2)/sizeof(data_2[0]));
-	Node<int>* list_3 = MakeNumberFrom2Lists<int>(list_1, list_2, true);	// 発展問題
-	Node<int>* list_4 = MakeNumberFrom2Lists<int>(list_1, list_2, false);	// 通常問題
+	char data_1[] = {1, 3, 5};
+	char data_2[] = {2, 4, 6};
 
-	Node<int>::Print(stdout, list_1);
-	Node<int>::Print(stdout, list_2);
-	Node<int>::Print(stdout, list_3);
-	Node<int>::Print(stdout, list_4);
+	Node<char>* input_1 = Node<char>::MakeLinkedList(data_1, sizeof(data_1)/sizeof(data_1[0]));
+	Node<char>* input_2 = Node<char>::MakeLinkedList(data_2, sizeof(data_2)/sizeof(data_2[0]));
 
-	Node<int>::DeleteList(list_1);
-	Node<int>::DeleteList(list_2);
-	Node<int>::DeleteList(list_3);
-	Node<int>::DeleteList(list_4);
+	Node<char>::PrintLinkedList(input_1);
+	Node<char>::PrintLinkedList(input_2);
+
+	Node<char>* output = ListSum<char>(input_1, input_2, true);
+	std::printf("=> ");
+	Node<char>::PrintLinkedList(output);
+	Node<char>::DeleteLinkedList(output);
+
+	output = ListSum<char>(input_1, input_2, false);
+	std::printf("=> ");
+	Node<char>::PrintLinkedList(output);
+	Node<char>::DeleteLinkedList(output);
+
+	Node<char>::DeleteLinkedList(input_1);
+	Node<char>::DeleteLinkedList(input_2);
 
 	return 0;
 }
