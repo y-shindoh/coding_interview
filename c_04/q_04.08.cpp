ae77aad01ac0928fb73a40af6dd35d87e1f389b8
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-02 19:01:13 +0900

    Modify an answer to Question 4.8.

--- c_04/q_04.08.cpp
+++ c_04/q_04.08.cpp
@@ -6,55 +6,45 @@
  * @note	see http://www.amazon.co.jp/dp/4839942390 .
  */
 
+/*
+  問題:
+  数百万のノードを持つT1と、数百のノードを持つT2の、2つの二分木があります。
+  このとき、T2がT1の部分木になっているかどうかを判定するアルゴリズムを作ってください。
+  T2がT1の部分木であるということは、T1上のあるノードnについて、
+  n以下の部分木がT2と同じであるということです。
+ */
+
 #include <cstddef>
 #include <cstdio>
 #include "binary_search_tree.hpp"
-
-/**
- * 比較関数
- */
-int
-compare(const int& left,
-		const int& right)
-{
-	if (left < right) {
-		return -1;
-	}
-	else if (left > right) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
-}
 
 /**
  * 動作確認用コマンド
  */
 int main()
 {
-	const int data_1[] = {0, 1, 2, 3, 4, 5, 6, 7};
-	const int data_2[] = {0, 1, 2};
-	BinarySearchTree<int>* tree_1 = new BinarySearchTree<int>(compare);
-	BinarySearchTree<int>* tree_2 = new BinarySearchTree<int>(compare);
+	const int data_1[] = {0, 1, 2, 3, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7};
+	const int data_2[] = {0, 1, 2, 3};
+	Node<int>* head_1;
+	Node<int>* head_2;
 
-	// 下記 BinarySearchTree::has_subtree<f> が回答
+	// 下記 Node<TYPE>::have_subtree<f> が回答
 
-	tree_1->prepare(data_1, sizeof(data_1)/sizeof(data_1[0]));
-	tree_1->print(stdout);
+	head_1 = Node<int>::Build(data_1, sizeof(data_1) / sizeof(data_1[0]));
+	head_1->print(stdout);
 
-	tree_2->prepare(data_2, sizeof(data_2)/sizeof(data_2[0]));
-	tree_2->print(stdout);
+	head_2 = Node<int>::Build(data_2, sizeof(data_2) / sizeof(data_2[0]));
+	head_2->print(stdout);
 
-	if (tree_1->has_subtree(*tree_2)) {
+	if (head_1->have_subtree(head_2)) {
 		std::printf("FOUND A SUBTREE!\n");
 	}
 	else {
 		std::printf("NOT FOUND ANY SUBTREES!\n");
 	}
 
-	delete tree_1;
-	delete tree_2;
+	delete head_1;
+	delete head_2;
 
 	return 0;
 }
