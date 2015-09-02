497c4a0b811b8cc721320fa7ecc5bbee10e6e8dd
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-02 18:59:56 +0900

    Modify an answer to Question 4.2.

--- c_04/q_04.02.cpp
+++ c_04/q_04.02.cpp
@@ -6,36 +6,99 @@
  * @note	see http://www.amazon.co.jp/dp/4839942390 .
  */
 
+/*
+  問題:
+  有向グラフが与えられたとき、
+  2つのノード間に経路があるかどうかを見つけるアルゴリズムを設計してください。
+ */
+
 #include <cstddef>
 #include <cstdio>
-#include <set>
 #include <vector>
+#include <set>
+#include <deque>
 
 /**
- * 探索処理
- * @param	table	有向グラフのテーブル
- * @param	start	始点
- * @param	goal	終点
- * @param	buffer	探索用作業領域
- * @note	深さ優先探索で実装している。無向グラフならunion-find treeが有利。
+ * 有向グラフ上の経路を探索 (補助関数)
+ * @param[in,out]	table	有向グラフ
+ * @param[in]	from	スタート
+ * @param[in]	to	ゴール
+ * @param[in,out]	buffer	通過済みノード (最初の呼び出しでは空にすること)
+ * @return	true: 経路あり, false: 経路なし
+ * @note	単純な深さ優先探索で実装している。
  */
 bool
-search(std::vector< std::set<size_t> >& table,
-	   size_t start,
-	   size_t goal,
-	   std::set<size_t>& buffer)
+search_dfs(const std::vector< std::set<size_t> >& table,
+		   size_t from,
+		   size_t to,
+		   std::set<size_t> buffer)
 {
-	if (start == goal) return true;
+	if (table[from].find(to) != table[from].end()) return true;
 
+	std::set<size_t>::const_iterator i;
 	bool flag;
-	std::set<size_t>::const_iterator it;
 
-	for (it = table[start].begin(); it != table[start].end(); ++it) {
-		if (buffer.find(*it) != buffer.end()) continue;
-		buffer.insert(*it);
-		flag = search(table, *it, goal, buffer);
-		buffer.erase(*it);
+	for (i = table[from].begin(); i != table[from].end(); ++i) {
+		if (buffer.find(*i) != buffer.end()) continue;
+		buffer.insert(*i);
+		flag = search_dfs(table, *i, to, buffer);
+		buffer.erase(*i);
 		if (flag) return true;
+	}
+
+	return false;
+}
+
+/**
+ * 有向グラフ上の経路を探索
+ * @param[in,out]	table	有向グラフ
+ * @param[in]	from	スタート
+ * @param[in]	to	ゴール
+ * @return	true: 経路あり, false: 経路なし
+ * @note	単純な深さ優先探索で実装している。
+ */
+bool
+search_dfs(const std::vector< std::set<size_t> >& table,
+		   size_t from,
+		   size_t to)
+{
+	std::set<size_t> buffer;
+	buffer.insert(from);
+
+	return search_dfs(table, from, to, buffer);
+}
+
+
+/**
+ * 有向グラフ上の経路を探索
+ * @param[in,out]	table	有向グラフ
+ * @param[in]	from	スタート
+ * @param[in]	to	ゴール
+ * @return	true: 経路あり, false: 経路なし
+ * @note	単純な幅優先探索で実装している。
+ */
+bool
+search_bfs(const std::vector< std::set<size_t> >& table,
+		   size_t from,
+		   size_t to)
+{
+	std::deque<size_t> buffer;
+	std::set<size_t> done;
+	std::set<size_t>::const_iterator it;
+	size_t i;
+
+	buffer.push_back(from);
+
+	while (!buffer.empty()) {
+		i = buffer.front();
+		buffer.pop_front();
+		for (it = table[i].begin(); it != table[i].end(); ++it) {
+			if (*it == from) continue;
+			if (*it == to) return true;
+			if (done.find(*it) != done.end()) continue;
+			buffer.push_back(*it);
+			done.insert(*it);
+		}
 	}
 
 	return false;
@@ -47,21 +110,28 @@ search(std::vector< std::set<size_t> >& table,
 int main()
 {
 	std::vector< std::set<size_t> > table;	// 有向グラフのテーブル
-	std::set<size_t> buffer;	// 探索用作業領域
 
 	table.resize(6);	// ノード数6 (0〜5)
 	table[0].insert(2);	// 0 => 2
 	table[2].insert(1);	// 2 => 1
 	table[1].insert(4);	// 1 => 4
 
-	if (!search(table, 0, 5, buffer)) {
-		std::printf("NG\n");	// 到達不可
+	if (!search_dfs(table, 0, 5)) {
+		std::printf("NG (DFS)\n");	// 到達不可
+	}
+
+	if (!search_bfs(table, 0, 5)) {
+		std::printf("NG (BFS)\n");	// 到達不可
 	}
 
 	table[4].insert(5);	// 4 => 5
 
-	if (search(table, 0, 5, buffer)) {
-		std::printf("OK\n");	// 到達可
+	if (search_dfs(table, 0, 5)) {
+		std::printf("OK (DFS)\n");	// 到達可
+	}
+
+	if (search_bfs(table, 0, 5)) {
+		std::printf("OK (BFS)\n");	// 到達可
 	}
 
 	return 0;
