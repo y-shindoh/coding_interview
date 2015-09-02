2ad68d5e52a2b95d57a5503c26d28789587a6a44
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-02 19:00:18 +0900

    Modify an answer to Question 4.7.

--- c_04/q_04.07.cpp
+++ c_04/q_04.07.cpp
@@ -6,56 +6,45 @@
  * @note	see http://www.amazon.co.jp/dp/4839942390 .
  */
 
+/*
+  問題:
+  二分木において、
+  2つのノードの上位ノードで最初に共通するものを探すアルゴリズムを設計し、
+  コードを書いてください。
+  ただし、データ構造の中に新たにノードを追加してはいけません
+  (二分木は二分探索木とは限りません)。
+ */
+
 #include <cstddef>
 #include <cstdio>
-#define	__BINARY_SEARCH_TREE_GET_RAW_DATA__	1
-#define	__BINARY_SEARCH_TREE_ENABLE_PARENT__	1
+#define	_USE_TRACE_BACK_TREE_	1
 #include "binary_search_tree.hpp"
-
-/**
- * 比較関数
- */
-int
-compare(const int& left,
-		const int& right)
-{
-	if (left < right) {
-		return -1;
-	}
-	else if (left > right) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
-}
 
 /**
  * 動作確認用コマンド
  */
 int main()
 {
-	const int data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-	const size_t l = sizeof(data)/sizeof(data[0]);
-	BinarySearchTree<int>* tree = new BinarySearchTree<int>(compare);
+	const int data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+	Node<int>* head(0);
 
-	tree->prepare(data, l);
+	head = Node<int>::Build(data, sizeof(data) / sizeof(data[0]));
 	std::printf("**tree**\n");
-	tree->print(stdout, 0);
+	head->print(stdout);
 
-	// 下記の BinarySearchNode<int>::FindSameParent が問題の回答
+	// 下記の Node<int>::FindMinimumSubtree が問題の回答
 
-	const BinarySearchNode<int>* node_1 = tree->search(3);
-	const BinarySearchNode<int>* node_2 = tree->search(8);
+	const Node<int>* node_1 = head->search(4);
+	const Node<int>* node_2 = head->search(0);
 	if (node_1 && node_2) {
-		const BinarySearchNode<int>* node = BinarySearchNode<int>::FindSameParent(node_1, node_2);
+		const Node<int>* node = Node<int>::FindMinimumSubtree(node_1, node_2);
 		if (node) {
 			std::printf(">>>> %d, %d => %d\n",
-						node_1->get_key(), node_2->get_key(), node->get_key());
+						node_1->get_data(), node_2->get_data(), node->get_data());
 		}
 	}
 
-	delete tree;
+	delete head;
 
 	return 0;
 }
