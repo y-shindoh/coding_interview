913f8fde33eac9f3a19259ac3df651767a2f2c91
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-03 19:16:22 +0900

    Modify an answer to Question 5.1.

--- c_05/q_05.01.cpp
+++ c_05/q_05.01.cpp
@@ -6,19 +6,36 @@
  * @note	see http://www.amazon.co.jp/dp/4839942390 .
  */
 
+/*
+  問題:
+  最大32ビットの整数NとM、ビットの位置を指す値iとjが与えられています。
+  このとき、Nのjビット目からiビット目にMを挿入するメソッドを書いてください。
+  ただし、jとiの幅はMのビット数と一致していると仮定してかまいません。
+  つまり、M=10011であればjとiの幅は5と仮定してかまいません。
+  j=3, i=2のような、Mの幅と合わないような場合は考えなくてもかまわないということです。
+ */
+
 #include <cstddef>
 #include <cstdio>
 #include "bits_operation.hpp"
 
+/**
+ * 整数をあるビットからあるビットまで他の整数のビットで置換
+ * @param[in]	base	置換元の整数
+ * @param[in]	add	置換先の整数
+ * @param[in]	i	置換開始ビットのインデックス
+ * @param[in]	j	置換終了ビットのインデックス
+ * @return	置換後の整数
+ * @note	little endian用の実装であることに注意。
+ */
 unsigned int
 replace_bits(unsigned int base,
 			 unsigned int add,
 			 size_t i,
 			 size_t j)
 {
-	base = clear_low_bits<unsigned int>(base, i) | clear_high_bits<unsigned int>(base, j);
-	add = clear_high_bits<unsigned int>(add, i - j + 1);
-	return base | (add << j);
+	base &= ((((size_t)1 << i) - (size_t)1) | (~((size_t)0) << (j + 1)));
+	return base | (~(~((size_t)0) << (j + 1)) & (add << i));
 }
 
 /**
@@ -27,12 +44,17 @@ replace_bits(unsigned int base,
 int main()
 {
 	unsigned int base(0xFF884433);
-	unsigned int add(0x00000CC0);
-	unsigned int value = replace_bits(base, add, 23, 8);
+	unsigned int add(0x19);
 
-	print_bits<unsigned int>(stdout, base);
-	print_bits<unsigned int>(stdout, add);
-	print_bits<unsigned int>(stdout, value);
+	size_t i (8);
+	size_t j (12);
+
+	unsigned int value = replace_bits(base, add, i, j);
+
+	print_bits<unsigned int>(stdout, base, "N:");
+	print_bits<unsigned int>(stdout, add, "M:");
+	std::printf("i = %lu, j = %lu\n", i, j);
+	print_bits<unsigned int>(stdout, value, "=>");
 
 	return 0;
 }
