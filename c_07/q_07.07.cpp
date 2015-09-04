f3ee3bc13a3e2573cc4b9ce3307955f23260fb1c
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-05 06:19:22 +0900

    Modify an answer to Question 7.7.

--- c_07/q_07.07.cpp
+++ c_07/q_07.07.cpp
@@ -14,9 +14,9 @@
 
 #include <cstddef>
 #include <cstdio>
-#include <cassert>
 #include <queue>
 #include <vector>
+#include <set>
 
 /**
  * 与えた因数から生成される合成数のうちk番目に小さいものを算出
@@ -27,24 +27,73 @@
  */
 template<typename TYPE>
 TYPE
-find_kth_composition(const TYPE* factors,
-					 size_t lenght,
-					 size_t k)
+find_kth_composition_1(const TYPE* factors,
+					   size_t lenght,
+					   size_t k)
 {
 	std::priority_queue< TYPE, std::vector<TYPE>, std::greater<TYPE> > queue;
-	TYPE value;
+	std::set<TYPE> post;
+	TYPE x, y;
 
 	queue.push((TYPE)1);
 
 	for (size_t i(0); i <= k; ++i) {	// インデックスに注意
-		value = queue.top();
+		x = queue.top();
 		for (size_t j(0); j < lenght; ++j) {
-			queue.push(value * factors[j]);
+			y = x * factors[j];
+			if (post.find(y) != post.end()) continue;
+			queue.push(y);
+			post.insert(y);
 		}
-		while (value >= queue.top()) queue.pop();	// 重複を除去
+		queue.pop();
 	}
 
 	return queue.top();
+}
+
+/**
+ * 与えた因数から生成される合成数のうちk番目に小さいものを算出
+ * @param[in]	factors	因数
+ * @param[in]	lenght	引数 @a factors の要素数
+ * @param[in]	k	昇順におけるインデックス (0始まり)
+ * @return	k番目に小さい合成数
+ * @note	型 @a TYPE には符号なし整数を指定すること。
+ */
+template<typename TYPE>
+TYPE
+find_kth_composition_2(const TYPE* factors,
+					   size_t lenght,
+					   size_t k)
+{
+	std::vector< std::queue<TYPE> > queue;
+	std::set<TYPE> post;
+
+	queue.resize(lenght);
+	queue[0].push(1);
+
+	TYPE x, y;
+	size_t h;
+
+	for (size_t i(0); ; ++i) {
+		// 最小値の探索
+		x = ~(TYPE)0;
+		for (size_t j(0); j < lenght; ++j) {
+			if (queue[j].empty() || x <= queue[j].front()) continue;
+			x = queue[j].front();
+			h = j;
+		}
+		if (k < i) break;	// インデックスに注意
+		queue[h].pop();
+		// 次の値の格納
+		for (size_t j(0); j < lenght; ++j) {
+			y = x * factors[j];
+			if (post.find(y) != post.end()) continue;
+			queue[j].push(y);
+			post.insert(y);
+		}
+	}
+
+	return x;
 }
 
 /**
@@ -56,7 +105,12 @@ int main()
 	unsigned int n;
 
 	for (unsigned int i(0); i < 9; ++i) {
-		n = find_kth_composition<unsigned int>(factors, sizeof(factors) / sizeof(factors[0]), i);
+		n = find_kth_composition_1<unsigned int>(factors, sizeof(factors) / sizeof(factors[0]), i);
+		printf("[%u] %u\n", i, n);
+	}
+
+	for (unsigned int i(0); i < 9; ++i) {
+		n = find_kth_composition_2<unsigned int>(factors, sizeof(factors) / sizeof(factors[0]), i);
 		printf("[%u] %u\n", i, n);
 	}
 
