6596df355041c1b707d6442c71139a59f4db5275
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-07 02:01:00 +0900

    Modify an answer to Question 9.6.

--- c_09/q_09.06.cpp
+++ c_09/q_09.06.cpp
@@ -6,45 +6,48 @@
  * @note	see http://www.amazon.co.jp/dp/4839942390 .
  */
 
+/*
+  問題:
+  n組の括弧の、括弧の対応が撮れた並び順すべてを表示するアルゴリズムを実装してください。
+ */
+
 #include <cstddef>
 #include <cstdio>
 #include <cassert>
 
 /**
- * 整合性のある括弧の組み合わせ全てを出力
- * @param	buffer	作業領域 (引数 @a pairs の2倍+1の長さが必要)
- * @param[in]	pairs	括弧のペアの個数
- * @param[in]	left	使用済み開き括弧の総数
- * @param[in]	right	使用済み閉じ括弧の総数
- * @param[in]	l_char	開き括弧の文字
- * @param[in]	r_char	閉じ括弧の文字
+ * 指定数の括弧の整合性のある全ての並びを出力
+ * @param[out]	file	出力先
+ * @param[in,out]	buffer	作業領域 (括弧の並ぶ文字列を書き出すのに十分な長さが必要)
+ * @param[in]	number	括弧のペアの数
+ * @param[in]	open	使用済み開き括弧の数
+ * @param[in]	close	使用済み閉じ括弧の数
+ * @note	計算量は O(2^number)。
  */
 void
-print_parens(char* buffer,
-			 size_t pairs,
-			 size_t left = 0,
-			 size_t right = 0,
-			 char l_char = '(',
-			 char r_char = ')')
+display_parens(FILE* file,
+			   char* buffer,
+			   size_t number,
+			   size_t open = 0,
+			   size_t close = 0)
 {
+	assert(file);
 	assert(buffer);
 
-	size_t i = left + right;
-
-	if (i == pairs * 2) {
-		buffer[i] = '\0';
-		std::printf("%s\n", buffer);
+	if (number <= open && number <= close) {
+		buffer[open+close] = '\0';
+		std::fprintf(file, "%s\n", buffer);
 		return;
 	}
 
-	if (left < pairs) {
-		buffer[i] = l_char;
-		print_parens(buffer, pairs, left + 1, right);
+	if (open < number) {
+		buffer[open+close] = '(';
+		display_parens(file, buffer, number, open+1, close);
 	}
 
-	if (right < pairs && right < left) {
-		buffer[i] = r_char;
-		print_parens(buffer, pairs, left, right + 1);
+	if (close < open) {
+		buffer[open+close] = ')';
+		display_parens(file, buffer, number, open, close+1);
 	}
 }
 
@@ -54,7 +57,7 @@ print_parens(char* buffer,
 int main()
 {
 	char buffer[7];
-	print_parens(buffer, 3);
+	display_parens(stdout, buffer, 3);
 
 	return 0;
 }
