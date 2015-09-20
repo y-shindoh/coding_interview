5449015d8f26d0943d74a87530319a619da0eada
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-20 11:38:04 +0900

    Modify an answer to Question 11.6.

--- c_11/q_11.06.cpp
+++ c_11/q_11.06.cpp
@@ -18,30 +18,30 @@
 #include <utility>
 
 /**
- * 行と列ともに昇順ソートされた正方行列から値を位置を探索
- * @param[in]	matrix	探索対象の正方行列
+ * 行と列ともに昇順ソートされた行列から値を位置を探索
+ * @param[in]	matrix	探索対象の行列
  * @param[in]	value	探索対象の値
  * @param[in]	i	探索開始インデックス (行)
  * @param[in]	j	探索開始インデックス (列)
  * @param[in]	h	探索終端インデックス (行, 1つ前のインデックスまで探索)
  * @param[in]	w	探索終端インデックス (列, 1つ前のインデックスまで探索)
- * @return	探索結果の行と列のインデックス (探索失敗時は @a N の組が返る)
- * @note	テンプレートの整数 @a N は正方行列の高さ・幅。
- * @note	最悪計算量は O(n log n) となる。
+ * @return	探索結果の行と列のインデックス (探索失敗時は @a M と @a N の組が返る)
+ * @note	テンプレートの整数 @a M, @a N はそれぞれ行列の高さ・幅。
+ * @note	最悪計算量は O(n log n) となる。ただし n は @a N とする。
  */
-template<typename TYPE, size_t N>
+template<typename TYPE, size_t M, size_t N>
 std::pair<size_t, size_t>
-find_in_matrix(const TYPE matrix[N][N],
+find_in_matrix(const TYPE matrix[M][N],
 			   TYPE value,
 			   size_t i = 0,
 			   size_t j = 0,
-			   size_t h = N,
+			   size_t h = M,
 			   size_t w = N)
 {
 	assert(matrix);
 	assert(i < h);
 	assert(j < w);
-	assert(h <= N);
+	assert(h <= M);
 	assert(w <= N);
 
 	size_t s(0);	// value より小さい要素の最大値 (初期値に注意)
@@ -71,19 +71,19 @@ find_in_matrix(const TYPE matrix[N][N],
 	// - 上記のいずれでもない場合、s + 1 == e となる。
 	//   もちろん、探索終了時を除く。
 
-	std::pair<size_t, size_t> result(N, N);
+	std::pair<size_t, size_t> result(M, N);
 
 	if (s < e) {
 		// 右上
 		if (j + e < w) {
 			result = find_in_matrix<TYPE, N>(matrix, value, i, j + e, i + e, w);
-			if (result.first != N && result.second != N) return result;
+			if (result.first != M && result.second != N) return result;
 		}
 
 		// 左下
 		if (i + e < h) {
 			result = find_in_matrix<TYPE, N>(matrix, value, i + e, j, h, j + e);
-			if (result.first != N && result.second != N) return result;
+			if (result.first != M && result.second != N) return result;
 		}
 	}
 
@@ -101,7 +101,7 @@ int main()
 						{40,  80, 100, 120}};
 	int value(40);
 
-	std::pair<size_t, size_t> result = find_in_matrix<int, 4>(matrix, value);
+	std::pair<size_t, size_t> result = find_in_matrix<int, 4, 4>(matrix, value);
 
 	if (result.first < 4) {
 		std::printf("FOUND: matrix[%lu][%lu] = %d\n", result.first, result.second, value);
