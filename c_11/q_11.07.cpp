226383011583ff37891ea8d76feece027da65b15
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-20 18:46:20 +0900

    Modify an answer to Question 11.7.

--- c_11/q_11.07.cpp
+++ c_11/q_11.07.cpp
@@ -26,7 +26,9 @@
  * @param[in]	data	サイズの組の配列
  * @param[in]	length	配列 @a data の要素数
  * @param[in]	index	起点要素のインデックス
- * @param[out]	buffer	計算領域 (長さは @a length 以上)
+ * @param[out]	indexes	各要素を始点とした連結した要素数が最大となる時の次の要素
+ * @param[out]	counts	各要素を始点とした連結した要素数の最大値
+ * @return	連結した要素が最長となる始点インデックス
  * @return	連結した要素の最大数
  * @note	テンプレートの型 @a TYPE は配列の要素の型。
  * @note	最悪計算量は O(n^2)。ただし n は @a length に等しい。
@@ -36,32 +38,39 @@ void
 find_longest_sequence(const std::pair<TYPE, TYPE>* data,
 					  size_t length,
 					  size_t index,
-					  size_t* buffer)
+					  size_t* indexes,
+					  size_t* counts)
 {
 	assert(data);
 	assert(0 < length);
 	assert(index < length);
-	assert(buffer);
+	assert(indexes);
+	assert(counts);
 
-	if (0 < buffer[index]) return;	// DP
+	if (0 < counts[index]) return;	// DP
+
+	indexes[index] = index;
 
 	for (size_t i(0); i < length; ++i) {
 		if (i == index) continue;	// 本来は不要
 		if (data[index].first <= data[i].first) continue;
 		if (data[index].second <= data[i].second) continue;
-		find_longest_sequence<TYPE>(data, length, i, buffer);
-		if (buffer[index] < buffer[i]) buffer[index] = buffer[i];
+		find_longest_sequence<TYPE>(data, length, i, indexes, counts);
+		if (counts[index] >= counts[i]) continue;
+		indexes[index] = i;
+		counts[index] = counts[i];
 	}
 
-	++buffer[index];
+	++counts[index];
 }
 
 /**
  * 連結する要素の最大数を算出
  * @param[in]	data	サイズの組の配列
  * @param[in]	length	配列 @a data の要素数
- * @param[out]	buffer	計算領域 (長さは @a length 以上)
- * @return	連結した要素の最大数
+ * @param[out]	indexes	各要素を始点とした連結した要素数が最大となる時の次の要素
+ * @param[out]	counts	各要素を始点とした連結した要素数の最大値
+ * @return	連結した要素が最長となる始点インデックス
  * @note	テンプレートの型 @a TYPE は配列の要素の型。
  * @note	最悪計算量は O(n^2)。ただし n は @a length に等しい。
  * @note	ソーティング後の配列を処理した方が効率的だが、
@@ -71,22 +80,25 @@ template<typename TYPE>
 size_t
 find_longest_sequence(const std::pair<TYPE, TYPE>* data,
 					  size_t length,
-					  size_t* buffer)
+					  size_t* indexes,
+					  size_t* counts)
 {
 	assert(data);
 	assert(0 < length);
-	assert(buffer);
+	assert(indexes);
+	assert(counts);
 
-	std::memset((void*)buffer, 0, sizeof(size_t) * length);
+	std::memset((void*)indexes, 0, sizeof(size_t) * length);
+	std::memset((void*)counts, 0, sizeof(size_t) * length);
 
 	size_t k(0);
 
 	for (size_t i(0); i < length; ++i) {
-		find_longest_sequence<TYPE>(data, length, i, buffer);
-		if (buffer[k] < buffer[i]) k = i;
+		find_longest_sequence<TYPE>(data, length, i, indexes, counts);
+		if (counts[k] < counts[i]) k = i;
 	}
 
-	return buffer[k];
+	return k;
 }
 
 /**
@@ -102,15 +114,24 @@ int main()
 												std::pair<size_t, size_t>(66, 160),
 												std::pair<size_t, size_t>(68, 110)};
 	const size_t l = sizeof(people) / sizeof(people[0]);
-	size_t buffer[sizeof(people) / sizeof(people[0])];
+	size_t indexes[sizeof(people) / sizeof(people[0])];
+	size_t counts[sizeof(people) / sizeof(people[0])];
 
 	for (size_t i(0); i < l; ++i) {
 		std::printf("[%lu] %lu, %lu\n", i, people[i].first, people[i].second);
 	}
 
-	size_t k = find_longest_sequence<size_t>(people, l, buffer);
+	size_t k = find_longest_sequence<size_t>(people, l, indexes, counts);
+	size_t c = counts[k];
 
-	std::printf("=> %lu\n", k);
+	std::printf("=> %lu (", c);
+
+	for (size_t i(0); i < c; ++i) {
+		if (0 < i) std::printf(", ");
+		std::printf("%lu", k);
+		k = indexes[k];
+	}
+	std::printf(")\n");
 
 	return 0;
 }
