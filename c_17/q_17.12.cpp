9d917048bbec18a24b3f92aacfef7ae3606ee9ae
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-10-25 02:45:46 +0900

    Fix the mistake about duplication.

--- c_17/q_17.12.cpp
+++ c_17/q_17.12.cpp
@@ -45,7 +45,7 @@ void find_sum_1(int sum,
 /**
  * 配列から指定した合計値になる要素の組を探索
  * @param[in]	sum	合計値
- * @param[in]	data	昇順ソート済み配列
+ * @param[in]	data	重複なし昇順ソート済み配列
  * @param[in]	length	配列 @a data の要素数
  * @note	計算量は O(n) になる。ただし n は引数 @a length に等しい。
 			ただし事前のソート処理が O(n log n) であることに注意。
@@ -63,14 +63,9 @@ void find_sum_2(int sum,
 
 	while (i < j) {
 		if (sum == data[i] + data[j]) {
-			m = n = 1;
-			while (i + m < j && data[i] == data[i+m]) ++m;
-			while (i + m < j - n && data[j] == data[j-n]) ++n;
-			for (size_t k(0); k < m * n; ++k) {
-				std::printf("(%d, %d)\n", data[i], data[j]);
-			}
-			i += m;
-			j -= n;
+			std::printf("(%d, %d)\n", data[i], data[j]);
+			++i;
+			--j;
 		}
 		else {
 			if (data[i] + data[j] < sum) ++i;
@@ -84,7 +79,7 @@ void find_sum_2(int sum,
  */
 int main()
 {
-	int data[] = {-2, -1, 0, 0, 3, 5, 5, 6, 7, 9, 13, 14};
+	int data[] = {-2, -1, 0, 3, 5, 6, 7, 9, 13, 14};
 	size_t length = sizeof(data) / sizeof(data[0]);
 
 	find_sum_1(5, data, length);
