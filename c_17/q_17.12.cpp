5619c31f8284b489207cd49907ee390fddc5bde2
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-10-25 00:58:50 +0900

    Add a new function.

--- c_17/q_17.12.cpp
+++ c_17/q_17.12.cpp
@@ -16,10 +16,11 @@
  * @param[in]	sum	合計値
  * @param[in]	data	配列
  * @param[in]	length	配列 @a data の要素数
+ * @note	計算量は O(n log n) になる。ただし n は引数 @a length に等しい。
  */
-void find_sum(int sum,
-			  const int* data,
-			  size_t length)
+void find_sum_1(int sum,
+				const int* data,
+				size_t length)
 {
 	assert(data);
 
@@ -36,6 +37,37 @@ void find_sum(int sum,
 }
 
 /**
+ * 配列から指定した合計値になる要素の組を探索
+ * @param[in]	sum	合計値
+ * @param[in]	data	昇順ソート済み配列
+ * @param[in]	length	配列 @a data の要素数
+ * @note	計算量は O(n) になる。ただし n は引数 @a length に等しい。
+			ただし事前のソート処理が O(n log n) であることに注意。
+ */
+void find_sum_2(int sum,
+				const int* data,
+				size_t length)
+{
+	assert(data);
+	assert(0 < length);
+
+	size_t i(0);
+	size_t j(length-1);
+
+	while (i < j) {
+		if (sum == data[i] + data[j]) {
+			std::printf("(%d, %d)\n", data[i], data[j]);
+			if (data[i+1] - data[i] < data[j] - data[j-1]) ++i;
+			else --j;
+		}
+		else {
+			if (data[i] + data[j] < sum) ++i;
+			else --j;
+		}
+	}
+}
+
+/**
  * 動作確認用コマンド
  * @note	単語は空白区切りで記述され、大文字小文字の違いを認める。
  */
@@ -44,7 +76,9 @@ int main()
 	int data[] = {-2, -1, 0, 3, 5, 6, 7, 9, 13, 14};
 	size_t length = sizeof(data) / sizeof(data[0]);
 
-	find_sum(5, data, length);
+	find_sum_1(5, data, length);
+	std::printf("----\n");
+	find_sum_2(5, data, length);
 
 	return 0;
 }
