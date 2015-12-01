d63f01f5cba555aee5f17a25c1a9e767c2233a0b
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-12-01 19:36:33 +0900

    Add comments.

--- c_05/q_05.02.cpp
+++ c_05/q_05.02.cpp
@@ -14,12 +14,14 @@
 
 #include <cstddef>
 #include <cstdio>
+#include <cassert>
 
 /**
  * 0以上1未満の実数を2進数表現に変換 (0.25 => "01", 0.125 => "001")
  * @param[in]	value	0以上1未満の実数
  * @param[out]	buffer	変換結果の格納先
  * @param[in]	l	変換結果の格納文字数
+ * @return	true: 変換成功, false: 変換失敗
  * @note	引数 @a buffer は引数 @a l の長さの文字列を扱える十分な広さの領域とすること。
  */
 bool
@@ -27,9 +29,14 @@ convert_to_binary(double value,
 				  char* buffer,
 				  size_t l = 32)
 {
+	assert(0.0 <= value);
+	assert(value < 1.0);
+	assert(buffer);
+
+	size_t i(0);
 	double divider = 1.0;
 
-	for (size_t i(0); i < l; ++i) {
+	while (i < l && 0.0 < value) {
 		divider /= 2.0;
 		if (divider <= value) {
 			value -= divider;
@@ -38,22 +45,25 @@ convert_to_binary(double value,
 		else {
 			buffer[i] = '0';
 		}
+		++i;
 	}
-	buffer[l] = '\0';
+	buffer[i] = '\0';
 
 	return value == 0.0;
 }
+
+#define	LENGTH	((size_t)32)
 
 /**
  * 動作確認用コマンド
  */
 int main()
 {
-	char buffer[33];
+	char buffer[LENGTH+1];
 	double values[] = {0.625, 0.0001, 0.0322265625};
 
 	for (size_t i(0); i < sizeof(values) / sizeof(values[0]); ++i) {
-		if (convert_to_binary(values[i], buffer)) {
+		if (convert_to_binary(values[i], buffer, LENGTH)) {
 			std::printf("%G => '%s'\n", values[i], buffer);
 		}
 		else {
